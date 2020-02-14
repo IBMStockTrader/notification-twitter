7627e95e9637d8a8cc83f133c8f8c91e7c90aedd
John Alcorn <jalcorn@us.ibm.com>
2020-02-13 22:03:49 -0500

    Use the trust store, not the key store, for cert

--- registerTwitterSSLCertificate.py
+++ registerTwitterSSLCertificate.py
@@ -1,6 +1,6 @@
-# [7/31/19 19:27:41:676 UTC] SSL certificate and key management > SSL configurations > NodeDefaultSSLSettings > Key stores and certificates > NodeDefaultKeyStore > Signer certificates > Retrieve from port
+# [7/31/19 19:27:41:676 UTC] SSL certificate and key management > SSL configurations > NodeDefaultSSLSettings > Key stores and certificates > NodeDefaultTrustStore > Signer certificates > Retrieve from port
 
-AdminTask.retrieveSignerFromPort('[-keyStoreName NodeDefaultKeyStore -keyStoreScope (cell):DefaultCell01:(node):DefaultNode01 -host api.twitter.com -port 443 -certificateAlias twitter -sslConfigName NodeDefaultSSLSettings -sslConfigScopeName (cell):DefaultCell01:(node):DefaultNode01 ]')
+AdminTask.retrieveSignerFromPort('[-keyStoreName NodeDefaultTrustStore -keyStoreScope (cell):DefaultCell01:(node):DefaultNode01 -host api.twitter.com -port 443 -certificateAlias twitter -sslConfigName NodeDefaultSSLSettings -sslConfigScopeName (cell):DefaultCell01:(node):DefaultNode01 ]')
 
 AdminConfig.save()
 
